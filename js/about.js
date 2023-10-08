55dfe32e80724de9862eb73e3c937e6e09380f87
Maulana Imanulhaq <98022263+Fancyyy21@users.noreply.github.com>
2023-10-08 17:05:46 +0700

    json

--- js/about.js
+++ js/about.js
@@ -1,16 +1,16 @@
 // Fungsi untuk mengambil dan menampilkan data dari JSON
 async function fetchData() {
     try {
-        const response = await fetch('https://raw.githubusercontent.com/Fancypedia/fancypedia-store/main/filejson/about.json'); // Ganti 'url_ke_file_json' dengan URL tempat file JSON Anda disimpan
+        const response = await fetch('../filejson/about.json'); // Ganti 'path/to/your/json/file.json' dengan path menuju file JSON Anda
         const data = await response.json();
 
         // Mengisi elemen HTML dengan data JSON
-        document.getElementById('aboutFancypediaDescription').textContent = data.aboutFancypediaDescription;
-        document.getElementById('themeDescription').textContent = data.themeDescription;
+        document.getElementById('aboutFancypediaDescription').textContent = data.servicesInfo.aboutFancypedia.description;
+        document.getElementById('themeDescription').textContent = data.servicesInfo.theme.description;
     } catch (error) {
         console.error('Error:', error);
     }
 }
 
-// Memanggil fungsi fetchData untuk mengambil data JSON
+// Memanggil fungsi fetchData untuk mengambil data JSON dan menampilkan ke dalam elemen HTML
 fetchData();
